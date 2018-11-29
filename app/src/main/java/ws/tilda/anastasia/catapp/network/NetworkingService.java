0b14e90f83f4a290bbd2dce674b2533cc629b709
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-29 20:37:48 +0200

    Edits

--- app/src/main/java/ws/tilda/anastasia/catapp/network/NetworkingService.java
+++ app/src/main/java/ws/tilda/anastasia/catapp/network/NetworkingService.java
@@ -11,38 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import ws.tilda.anastasia.catapp.BuildConfig;
 
 public class NetworkingService {
-//    public static String apiBaseUrl = "http://biotope.serval.uni.lu:8383/";
-//
-//    private static Retrofit.Builder builder =
-//            new Retrofit.Builder()
-//                    .baseUrl(apiBaseUrl);
-//
-//    private static Retrofit retrofit = builder.build();
-//
-//    private static HttpLoggingInterceptor logging =
-//            new HttpLoggingInterceptor()
-//                    .setLevel(HttpLoggingInterceptor.Level.BODY);
-//
-//    private static OkHttpClient.Builder httpClient =
-//            new OkHttpClient.Builder();
-//
-//    public static <S> S createService(
-//            Class<S> serviceClass) {
-//        if (!httpClient.interceptors().contains(logging)) {
-//            httpClient.addInterceptor(logging);
-//        }
-//
-//        OkHttpClient okHttpClient = httpClient.connectTimeout(20, TimeUnit.SECONDS)
-//                .writeTimeout(20, TimeUnit.SECONDS)
-//                .readTimeout(20, TimeUnit.SECONDS)
-//                .build();
-//
-//        builder.client(okHttpClient);
-//
-//        retrofit = builder.build();
-//
-//        return retrofit.create(serviceClass);
-//    }
 
     private static OkHttpClient sClient;
     private static Retrofit sRetrofit;
