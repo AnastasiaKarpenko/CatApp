4a225c3491f5bee8a8631894e5ea30e8a181f8d8
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-11-29 20:36:11 +0200

    Adding headers to CatApi (Api key + content-type), Adding networking service generator, adding ApiKey and base url to the BuildConfig in gradle app file

--- app/src/main/java/ws/tilda/anastasia/catapp/network/CatApi.java
+++ app/src/main/java/ws/tilda/anastasia/catapp/network/CatApi.java
@@ -1,10 +1,17 @@
 package ws.tilda.anastasia.catapp.network;
 
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
+import ws.tilda.anastasia.catapp.BuildConfig;
 import ws.tilda.anastasia.catapp.model.CatResponse;
 
 public interface CatApi {
+    @Headers({
+            "Content-Type: application/json",
+            "x-api-key: " + BuildConfig.API_KEY
+    })
     @GET("/images/search")
     CatResponse getAllCats(@Query("size") String size,
                            @Query("mime_types") String mimeTypes,
