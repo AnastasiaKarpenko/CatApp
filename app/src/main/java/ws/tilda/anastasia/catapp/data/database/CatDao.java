3b6af3be16f0dfd9bf804a599c49ba9e0e131d6c
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-12-01 15:18:56 +0200

    Created abstract class of CatDatabase

---
+++ app/src/main/java/ws/tilda/anastasia/catapp/data/database/CatDao.java
@@ -0,0 +1,7 @@
+package ws.tilda.anastasia.catapp.data.database;
+
+import android.arch.persistence.room.Dao;
+
+@Dao
+public interface CatDao {
+}
---
+++ app/src/main/java/ws/tilda/anastasia/catapp/data/database/CatDatabase.java
@@ -0,0 +1,12 @@
+package ws.tilda.anastasia.catapp.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import ws.tilda.anastasia.catapp.data.model.Cat;
+
+@Database(entities = {Cat.class}, version = 1)
+public abstract class CatDatabase extends RoomDatabase {
+
+    public abstract CatDao getCatDao();
+}
