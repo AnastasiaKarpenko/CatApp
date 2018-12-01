f9101e70cae43ed82752706fdd34a38243d52a8f
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-12-01 15:42:13 +0200

    Implemented methods in the Repository to getAllCats from the database and to insert cats to the database. Created interface of RepositoryOwner in Repository class.

--- app/src/main/java/ws/tilda/anastasia/catapp/data/repository/Repository.java
+++ app/src/main/java/ws/tilda/anastasia/catapp/data/repository/Repository.java
@@ -1,11 +1,28 @@
 package ws.tilda.anastasia.catapp.data.repository;
 
+import java.util.List;
+
 import ws.tilda.anastasia.catapp.data.database.CatDao;
+import ws.tilda.anastasia.catapp.data.model.Cat;
 
 public class Repository {
     private CatDao mCatDao;
 
     public Repository(CatDao catDao) {
         mCatDao = catDao;
+    }
+
+    public List<Cat> getAllCats() {
+        List<Cat> cats = mCatDao.getAllCats();
+        return cats;
+    }
+
+    public void insertCats(List<Cat> cats) {
+        mCatDao.insertCats(cats);
+
+    }
+
+    public interface RepositoryOwner {
+        Repository obtainRepository();
     }
 }
