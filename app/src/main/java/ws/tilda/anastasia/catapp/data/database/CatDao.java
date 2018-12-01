650c7458904a4d84b1167f0ab52aab635c9a9395
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-12-01 15:28:45 +0200

    Setting up CatDao with two methods: to get all cats and to insert cats

--- app/src/main/java/ws/tilda/anastasia/catapp/data/database/CatDao.java
+++ app/src/main/java/ws/tilda/anastasia/catapp/data/database/CatDao.java
@@ -1,7 +1,20 @@
 package ws.tilda.anastasia.catapp.data.database;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import ws.tilda.anastasia.catapp.data.model.Cat;
 
 @Dao
 public interface CatDao {
+
+    @Query("select * from cat")
+    List<Cat> getAllCats();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertCats(List<Cat> cats);
 }
